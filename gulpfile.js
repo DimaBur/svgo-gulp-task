464b820970efb460d599b113689499338d5745f7
DimaBur <buravtsov.dm@yandex.ru>
2017-12-15 12:28:37 +0300

    set settings

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp');
+var svgmin = require('gulp-svgmin');
+
+gulp.task('default', function () {
+  return gulp.src('input/*.svg')
+      .pipe(svgmin({
+        plugins: [{
+          cleanupAttrs: true
+        }, {
+          removeDoctype: true
+        },{
+          removeXMLProcInst: true
+        },{
+          removeComments: true
+        },{
+          removeMetadata: true
+        },{
+          removeTitle: false//
+        },{
+          removeDesc: true
+        },{
+          removeUselessDefs: true
+        },{
+          removeEditorsNSData: true
+        },{
+          removeEmptyAttrs: true
+        },{
+          removeHiddenElems: true
+        },{
+          removeEmptyText: true
+        },{
+          removeEmptyContainers: true
+        },{
+          removeViewBox: false//
+        },{
+          cleanUpEnableBackground: true
+        },{
+          convertStyleToAttrs: true
+        },{
+          convertColors: true
+        },{
+          convertPathData: true
+        },{
+          convertTransform: true
+        },{
+          removeUnknownsAndDefaults: true
+        },{
+          removeNonInheritableGroupAttrs: true
+        },{
+          removeUselessStrokeAndFill: true
+        },{
+          removeUnusedNS: true
+        },{
+          cleanupIDs: {
+            force: true
+          },
+        },{
+          addAttributesToSVGElement: {
+            attribute: 'class="avatar_svg"'
+          },
+        },{
+          cleanupNumericValues: true
+        },{
+          moveElemsAttrsToGroup: true
+        },{
+          moveGroupAttrsToElems: true
+        },{
+          collapseGroups: true
+        },{
+          removeRasterImages: false//
+        },{
+          mergePaths: true
+        },{
+          convertShapeToPath: true
+        },{
+          sortAttrs: true//
+        },{
+          transformsWithOnePath: true//
+        },{
+          removeDimensions: true//
+        },{
+          removeAttrs: true//
+        },]
+      }))
+      .pipe(gulp.dest('./output'));
+});
